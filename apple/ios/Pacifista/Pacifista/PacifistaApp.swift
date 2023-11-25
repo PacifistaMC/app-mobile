f9ce2ffa547a6b073bd79f5e0d2ee7e0987b69fe
Antoine PRONNIER <antoine.pronnier@gmail.com>
2023-11-25 12:49:54 +0100

    init mobile app ios

---
+++ apple/ios/Pacifista/Pacifista/PacifistaApp.swift
@@ -0,0 +1,17 @@
+//
+//  PacifistaApp.swift
+//  Pacifista
+//
+//  Created by Antoine Pronnier on 25/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct PacifistaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
